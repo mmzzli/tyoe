51fc9f2d5ea7e2700657dd1a689132d4dbba8ed4
mmzzli <mmzzline@gmail.com>
2024-11-16 17:47:49 +0800

    fix web

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
   return (
     <>
       {user}
-      <RouterProvider router={router}></RouterProvider>
+      {/*<RouterProvider router={router}></RouterProvider>*/}
     </>
   )
 }
