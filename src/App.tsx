f0ee683fdc1ef22e8615628f39feac5181fb0143
gebo <gebo@newhuotech.com>
2024-11-26 11:04:16 +0800

    feat: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
-import {useEffect} from 'react'
-import './App.scss'
-import {RouterProvider} from "react-router-dom";
-import router from "./router";
-import WebApp from '@twa-dev/sdk'
-import useUserStore from "@/store/user.ts";
-const {MODE, VITE_APP_USER}  = import.meta.env
-const modes = ['production','staking']
-const isProd = modes.includes(MODE)
+import { useEffect } from 'react';
+import './App.scss';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
+import WebApp from '@twa-dev/sdk';
+import useUserStore from '@/store/user.ts';
+const { MODE, VITE_APP_USER } = import.meta.env;
+const modes = ['production', 'staking'];
+const isProd = modes.includes(MODE);
 function App() {
-  const setUser = useUserStore(s=>s.setUser)
+  const setUser = useUserStore((s) => s.setUser);
 
-  useEffect(()=>{
+  useEffect(() => {
     WebApp.ready();
-    let user:any = null;
-    if(!isProd){
+    let user: any = null;
+    if (!isProd) {
       user = JSON.parse(VITE_APP_USER).user;
-      console.log(user,'====');
-    }else{
-      user = WebApp.initDataUnsafe.user
+      console.log(user, '====');
+    } else {
+      user = WebApp.initDataUnsafe.user;
+
+      console.log('user ', user);
     }
     if (user) {
-      setUser(user)
+      setUser(user);
     }
-  })
+  });
   return (
     <>
       <RouterProvider router={router}></RouterProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
